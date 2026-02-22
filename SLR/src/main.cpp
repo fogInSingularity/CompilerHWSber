9769a08d8dc997b592852d8b25e1dc6b166cc2bc
fogInSingularity <vladimirnaumov979@gmail.com>
2026-02-22 10:34:02 +0300

    init

---
+++ SLR/src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include <spdlog/spdlog.h>
+#include <spdlog/common.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+int main(int argc, const char* argv[]) {
+    auto logger = spdlog::basic_logger_mt("slr", "slr.log", true);
+    spdlog::set_default_logger(logger);
+
+    spdlog::set_pattern("[%l] %v"); // remove time and name(%n) from log
+
+#if defined (NDEBUG)
+    spdlog::set_level(spdlog::level::info);
+#else // NDEBUG
+    // spdlog::flush_on(spdlog::level::trace);
+    spdlog::set_level(spdlog::level::debug);
+#endif // NDEBUG
+
+    // log argv
+    for (int i = 0; i < argc; i++) {
+        spdlog::info("argv[{}]: {}", i, argv[i]);
+    }
+
+    if (argc < 2) {
+        std::cerr << 
+            "ERROR: not enough args\n" 
+            "   usage: " << argv[0]  << " <text_path>\n";
+
+        return EXIT_FAILURE;
+    }
+}
